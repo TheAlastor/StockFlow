35a78a4a034d1492ea88ea556ba983e57ac16450
TheAlastor <50780317+TheAlastor@users.noreply.github.com>
2024-06-26 00:16:07 -0300

    Segundo commit

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import './styles.css'
 import { FiLogIn } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
 
 export default function Logon() {
   return (
     <div className="logon-page-container">
       <div className="header-container">
+        <section className="home-link">
+          <Link to="http://localhost:3000/">StockFlow</Link>
+        </section>
         <section className="navigation">
-          <a class="active" href="#home">
-            Home
-          </a>
-          <a href="#news">News</a>
-          <a href="#contact">Contact</a>
-          <a href="#about">About</a>
+          <Link to="#home">Home</Link>
+          <Link to="#news">News</Link>
+          <Link to="#contact">Contact</Link>
+          <Link to="#about">About</Link>
         </section>
         <section className="profile">teste</section>
       </div>
@@ -24,9 +26,9 @@ export default function Logon() {
             <h2>Login</h2>
             <h3>Enter your e-mail to sign in</h3>
             <input placeholder="email@modec.com" />
-            <input placeholder="password" />
+            <input placeholder="password" type="password" />
             <button className="button">Login</button>
-            <a>Forgot password</a>
+            <Link to="#home">Forgot password</Link>
             <h5>OR</h5>
             <FiLogIn size={16} color="blue" />
             <input placeholder="email@modec.com" />
