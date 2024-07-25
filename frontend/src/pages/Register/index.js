dbf4d492d4c3aa513d6e7898759ef6279179652f
TheAlastor <50780317+TheAlastor@users.noreply.github.com>
2024-07-25 17:31:17 -0300

    register 2

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -2,87 +2,123 @@ import React, { useState } from 'react'
 import './styles.css'
 import { FiLogIn } from 'react-icons/fi'
 import { Link } from 'react-router-dom'
-import userImage from 'C:/Users/vinic/Desktop/StockFlow/frontend/src/user.png'
+import userImage from 'C:/Users/Vinicius/Desktop/StockFlow/frontend/src/user.png'
 
 export default function Register() {
-  const [file, setFile] = useState()
-  function handleChange(e) {
-    setFile(URL.createObjectURL(e.target.files[0]))
-  }
+    
 
-  function handleButtonClick() {
-    document.getElementById('actual-btn').click()
-  }
+    /////////////// Get First and Last Name for Profile Preview /////
+    const getLastWord = (phrase) => {
+        const words = phrase.trim().split(' ');
+        return words[words.length - 1];
+    };
 
-  return (
-    <div className="register-page-container">
-      <form className="header-container">
-        <div className="home">
-          <Link to="http://localhost:3000/">StockFlow</Link>
-        </div>
+    const getFirstWord = (phrase) => {
+        return phrase.split(' ')[0];
+    };
 
-        <div className="navigation">
-          <Link to="#news">News</Link>
-          <Link to="#contact">Contact</Link>
-          <Link to="#about">About</Link>
-        </div>
+    /////////////// Change Last Name of Profile Preview /////
+    const [nameInput, setNameInput] = useState()
+    function handleInputNameChange(e) {        
+        setNameInput(getFirstWord(e.target.value))
+    }
 
-        <div className="profile">
-          <Link to="#profile">wProfile</Link>
-        </div>
-      </form>
+    ////////////////// Change Name of Profile Preview ////////
+    const [lastNameInput, setLastNameInput] = useState()
+    function handleInputLastNameChange(e) {        
+        setLastNameInput(getLastWord(e.target.value))
+    }
 
-      <div className="register-container">
-        <h1>StockFlow </h1>
-        <section className="form-register">
-          <div className="register-colum1">
-            <form>
-              <h2>Name</h2>
-              <input placeholder="Name" />
-              <h2>Personal E-mail</h2>
-              <input placeholder="email@modec.com" />
-              <h2>Password</h2>
-              <input placeholder="password" type="password" />
+    ////////////////// Call Input Image ///////////////////////
+    const [file, setFile] = useState(userImage)
+    function handleChange(e) {
+        setFile(URL.createObjectURL(e.target.files[0]))
+    }
+
+    function handleButtonClick() {
+        document.getElementById('actual-btn').click()
+    }
+
+    /////////////////////////////////////////////////////////
+
+
+    return (
+        <div className="register-page-container">
+            <form className="header-container">
+                <div className="home">
+                    <Link to="http://localhost:3000/">StockFlow</Link>
+                </div>
+
+                <div className="navigation">
+                    <Link to="#news">News</Link>
+                    <Link to="#contact">Contact</Link>
+                    <Link to="#about">About</Link>
+                </div>
+
+                <div className="profile">
+                    <Link to="#profile">Profile</Link>
+                </div>
             </form>
-          </div>
 
-          <div className="register-colum2">
-            <form>
-              <h2>Last Name</h2>
-              <input placeholder="Last Name" />
-              <h2>Onboard Position E-mail</h2>
-              <input placeholder="email@modec.com" />
-              <h2>Repeat Password</h2>
-              <input placeholder="repeat password" type="password" />
-            </form>
-          </div>
-        </section>
+            <div className="register-container">
+                <h1>StockFlow </h1>
+                <section className="form-register">
+                    <div className="register-colum1">
+                        <form>
+                            <h2>Name</h2>
+                            <input placeholder="Name" onChange={handleInputNameChange} />
+                            <h2>Personal E-mail</h2>
+                            <input placeholder="email@modec.com" />
+                            <h2>Password</h2>
+                            <input placeholder="password" type="password" />
+                        </form>
+                    </div>
 
-        <div className="register-upload">
-          <h3>This is how your profile will be displayed:</h3>
+                    <div className="register-colum2">
+                        <form>
+                            <h2>Last Name</h2>
+                            <input placeholder="Last Name" onChange={handleInputLastNameChange} />
+                            <h2>Onboard Position E-mail</h2>
+                            <input placeholder="email@modec.com" />
+                            <h2>Repeat Password</h2>
+                            <input placeholder="repeat password" type="password" />
+                        </form>
+                    </div>
+                </section>
 
-          <input
-            placeholder="select image"
-            type="file"
-            accept="image/*"
-            className="picture-input"
-            onChange={handleChange}
-            id="actual-btn"
-          />
+                <div className="register-upload">
+                    <h3>This is how your profile will be displayed:</h3>
 
-          <div className="user-profile">
-            <div className="user-picture">
-              <img src={userImage} className="circular-image" />
+                    <input
+                        placeholder="select image"
+                        type="file"
+                        accept="image/*"
+                        className="picture-input"
+                        onChange={handleChange}
+                        id="actual-btn"
+                    />
+
+                    <div className="user-profile">
+
+                        <div className="user-picture">
+                            <img src={file} className="circular-image" />
+                        </div>
+
+                        <div className="user-name">
+                            <form>
+                                <h3>{nameInput} {lastNameInput}</h3>                                
+                            </form>
+                        </div>
+
+                    </div>
+
+                    <button className="button-upload" onClick={handleButtonClick}>
+                        Upload<br></br>
+                        Photo<br></br>
+                    </button>
+                </div>
+                <button className="button-create">Create Account</button>
             </div>
-            User Name
-          </div>
-
-          <button className="button-upload" onClick={handleButtonClick}>
-            Upload Photo
-          </button>
         </div>
-        <button className="button">Create Account</button>
-      </div>
-    </div>
-  )
+    )
 }
